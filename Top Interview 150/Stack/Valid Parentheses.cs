f5d352d943ffeb54a4c1efada6368723b6490e8a
Darkerys <amirehsank3@gmail.com>
2025-01-12 13:21:28 +0330

    First Problem Solved

---
+++ Top Interview 150/Stack/Valid Parentheses.cs
@@ -0,0 +1,28 @@
+﻿namespace Top_Interview_150.Stack;
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        List<char> open = ['(', '[', '{'];
+        List<char> close = [')', ']', '}'];
+
+        var stack = new Stack<char>();
+        foreach (var item in s)
+        {
+            if (open.Contains(item))
+            {
+                stack.Push(item);
+            }
+            if (close.Contains(item))
+            {
+                if (stack.Count == 0)
+                    return false;
+                var top=stack.Pop();
+                if(open.IndexOf(top) != close.IndexOf(item))
+                    return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
