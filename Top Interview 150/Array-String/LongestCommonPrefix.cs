81b05e54280570f0b9c8cb0c15872801fc9b9f9d
Darkerys <amirehsank3@gmail.com>
2025-08-22 18:12:28 +0330

    Longest Common Prefix

---
+++ Top Interview 150/Array-String/LongestCommonPrefix.cs
@@ -0,0 +1,21 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class LongestCommonPrefix
+{
+    public string LongestCommonPrefixS(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+            return "";
+        string prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (!strs[i].StartsWith(prefix))
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix == "") return "";
+            }            
+        }
+        
+        return prefix;
+    }
+}
