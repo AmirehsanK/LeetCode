e818f464e378ba88790117d7d377254438dea054
Darkerys <amirehsank3@gmail.com>
2025-08-22 12:30:24 +0330

    Remove Duplicates from Sorted Arrays

---
+++ Top Interview 150/Array-String/RemoveDupSortedArray.cs
@@ -0,0 +1,22 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class RemoveDupSortedArray
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        var writer = 0;
+        if(nums.Length == 0)
+            return writer;
+        for (int reader = 1; reader < nums.Length; reader++)
+        {
+            if (nums[reader] != nums[writer])
+            {
+                writer++;
+                nums[writer] = nums[reader];
+
+            }
+        }
+        return writer+1;
+    }
+}
+/*Two Pointers used to solve this problem*/
