84e7fd208ea264c98dd777e571503c07c180d521
Darkerys <amirehsank3@gmail.com>
2025-08-22 18:34:44 +0330

    Find the index of the first occurence in a string

---
+++ Top Interview 150/Array-String/FindIndexFirstOccur.cs
@@ -0,0 +1,21 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class FindIndexFirstOccur
+{
+   public int StrStr(string haystack, string needle) 
+   {
+      int m = haystack.Length, n = needle.Length;
+      if (n > m)
+         return -1;
+
+      for (int i = 0; i <= m - n; i++)
+      {
+         int j = 0;
+         while (j < n && haystack[i + j] == needle[j])
+            j++;
+         if (j == n) return i;
+      }
+
+      return -1;
+   }
+}
