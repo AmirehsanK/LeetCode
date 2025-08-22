f234fc0d1f4e05a5ac339ac683205bc00cb8995d
Darkerys <amirehsank3@gmail.com>
2025-08-22 14:03:43 +0330

    Lenght of Last Word

---
+++ Top Interview 150/Array-String/LengthofLastWord.cs
@@ -0,0 +1,25 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class LengthofLastWord
+{
+    public int LengthOfLastWord(string s)
+    {
+        var i = s.Length - 1;
+        var count = 0;
+
+        while (i >= 0)
+        {
+            if (s[i] != ' ')
+            {
+                count++;
+            }
+            else if (count > 0)
+            {
+                break; 
+            }
+            i--;
+        }
+        return count;
+    }
+}
+//Reverse looping
