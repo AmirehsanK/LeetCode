4ea86b05853fed5f872513959d2fde03b6754f31
Darkerys <amirehsank3@gmail.com>
2025-08-22 19:39:24 +0330

    Valid Palindrome

---
+++ Top Interview 150/Array-String/ValidPalindrome.cs
@@ -0,0 +1,31 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        var newStr = new string(
+            s.Where(char.IsLetterOrDigit)
+                .Select(char.ToLowerInvariant)
+                .ToArray()
+        );
+        
+        var l = 0;
+        var r = newStr.Length-1;
+        
+        while (l < r)
+        {
+            if (newStr[l] == newStr[r])
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
