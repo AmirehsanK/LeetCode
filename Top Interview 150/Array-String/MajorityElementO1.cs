1ceda88f1e50e4835f805663a6ac41ceeae30e3c
Darkerys <amirehsank3@gmail.com>
2025-08-22 13:06:25 +0330

    Majority Element

---
+++ Top Interview 150/Array-String/MajorityElementO1.cs
@@ -0,0 +1,28 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class MajorityElementO1
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = 0, count = 0;
+        foreach (var num in nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+                count++;
+            }
+            else if(candidate==num)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+}
+/*Boyer–Moore implementation*/
