c909601042537c5399c3f54ef5f791f1b0676482
Darkerys <amirehsank3@gmail.com>
2025-08-22 13:21:42 +0330

    Best Time to Buy and Sell Stock 1

---
+++ Top Interview 150/Array-String/BestTimetoBuyandSellStock1.cs
@@ -0,0 +1,18 @@
+﻿namespace Top_Interview_150.Array_String;
+
+public class BestTimetoBuyandSellStock1
+{
+    public int MaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+        var lowest=prices[0];
+        foreach (var price in prices)
+        {
+            if (lowest > price)
+                lowest = price;
+            else if(price-lowest > maxProfit)
+                maxProfit = price-lowest;
+        }
+        return maxProfit;
+    }
+}
